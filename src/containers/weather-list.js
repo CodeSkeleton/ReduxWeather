cc01d753c135d130ef0fe91cd343c6ce2f684cb8
Yassine Zaroui <yassine.zaroui@elab-bv.com>
2016-04-05 17:50:22 +0200

    Tab size to 4 chars instead of 2

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -4,59 +4,62 @@ import Chart from '../components/chart';
 import GoogleMap from '../components/google-map';
 
 class WeatherList extends Component {
-  //constructor() {
-  //  super();
-  //  this.renderWeather = this.renderWeather.bind(this);
-  //}
 
-  render() {
-    return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (°C)</th>
-            <th>Pressure (hPa)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>
-         { this.props.weather.map(this.renderWeather)}
-        </tbody>
-      </table>
-    );
-  }
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                <tr>
+                    <th>City</th>
+                    <th>Temperature (°C)</th>
+                    <th>Pressure (hPa)</th>
+                    <th>Humidity (%)</th>
+                </tr>
+                </thead>
+                <tbody>
+                { this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        );
+    }
 
-  renderWeather(cityData) {
-    const cityName = cityData.city.name;
-    const temperature = cityData.list.map(item => item.main.temp);
-    const pressure = cityData.list.map(item => item.main.pressure);
-    const humidity = cityData.list.map(item => item.main.humidity);
-    const {lat, lon} = cityData.city.coord;
+    renderWeather(cityData) {
+        let temperature = [];
+        let pressure = [];
+        let humidity = [];
 
-    return (
-      <tr key={cityName}>
-        <td>
-          <GoogleMap lat={lat} lon={lon} />
-          <div>{cityName}</div>
-        </td>
-        <td>
-          <Chart data={temperature} unit="°C" width={250} height={100} min={-20} max={50} color="orange"/>
-        </td>
-        <td>
-          <Chart data={pressure} unit="hPa" width={250} height={100} min={800} max={1050} color="green"/>
-        </td>
-        <td>
-          <Chart data={humidity} unit="%" width={250} height={100} min={0} max={100} color="blue"/>
-        </td>
-      </tr>
-    );
-  }
+        cityData.list.forEach((weather) => {
+            temperature.push(weather.main.temp);
+            pressure.push(weather.main.pressure);
+            humidity.push(weather.main.humidity);
+        });
+
+        let cityName = cityData.city.name;
+        let {lat, lon} = cityData.city.coord;
+
+        return (
+            <tr key={cityName}>
+                <td>
+                    <GoogleMap lat={lat} lon={lon}/>
+                    <div>{cityName}</div>
+                </td>
+                <td>
+                    <Chart data={temperature} unit="°C" width={250} height={100} min={-20} max={50} color="orange"/>
+                </td>
+                <td>
+                    <Chart data={pressure} unit="hPa" width={250} height={100} min={800} max={1050} color="green"/>
+                </td>
+                <td>
+                    <Chart data={humidity} unit="%" width={250} height={100} min={0} max={100} color="blue"/>
+                </td>
+            </tr>
+        );
+    }
 }
 
 
 function mapStateToProps({weather}) {
-  return { weather };
+    return {weather};
 }
 
 export default connect(mapStateToProps)(WeatherList);
